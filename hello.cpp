b884d6d27bbb5bdf715cbaf88023c9fface3a1a6
ᴇʀʀᴏʀ 404 <123000318+tdc9@users.noreply.github.com>
2024-09-13 20:30:04 +0530

    idk

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ int main() {
     std::string name;
     std::cout << "Whats You Name Hooman? :";
     std::getline(std::cin >> std::ws, name);
-    std::cout << "Good Hooman " << name;
+    std::cout << "Good Hooman " << name <<'\n';
 
     
     return 0;
